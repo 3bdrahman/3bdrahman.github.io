11a388e33d8fef41921a24a307ff22a3f5e4393a
3bdrahman <35944185+3bdrahman@users.noreply.github.com>
2018-02-06 19:19:09 +0200

    First website upload

---
+++ assets/js/main.js
@@ -0,0 +1,144 @@
+/*
+	Spectral by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function ($) {
+
+    skel
+        .breakpoints({
+            xlarge: '(max-width: 1680px)',
+            large: '(max-width: 1280px)',
+            medium: '(max-width: 980px)',
+            small: '(max-width: 736px)',
+            xsmall: '(max-width: 480px)'
+        });
+
+    $(function () {
+
+        var $window = $(window),
+            $body = $('body'),
+            $wrapper = $('#page-wrapper'),
+            $banner = $('#banner'),
+            $header = $('#header');
+
+        // Disable animations/transitions until the page has loaded.
+        $body.addClass('is-loading');
+
+        $window.on('load', function () {
+            window.setTimeout(function () {
+                $body.removeClass('is-loading');
+            }, 100);
+        });
+
+        // Mobile?
+        if (skel.vars.mobile)
+            $body.addClass('is-mobile');
+        else
+            skel
+            .on('-medium !medium', function () {
+                $body.removeClass('is-mobile');
+            })
+            .on('+medium', function () {
+                $body.addClass('is-mobile');
+            });
+
+        // Fix: Placeholder polyfill.
+        $('form').placeholder();
+
+        // Prioritize "important" elements on medium.
+        skel.on('+medium -medium', function () {
+            $.prioritize(
+                '.important\\28 medium\\29',
+                skel.breakpoint('medium').active
+            );
+        });
+
+        // Scrolly.
+        $('.scrolly')
+            .scrolly({
+                speed: 1500,
+                offset: $header.outerHeight()
+            });
+
+        // Menu.
+        $('#menu')
+            .append('<a href="#menu" class="close"></a>')
+            .appendTo($body)
+            .panel({
+                delay: 500,
+                hideOnClick: true,
+                hideOnSwipe: true,
+                resetScroll: true,
+                resetForms: true,
+                side: 'right',
+                target: $body,
+                visibleClass: 'is-menu-visible'
+            });
+
+        // Header.
+        if (skel.vars.IEVersion < 9)
+            $header.removeClass('alt');
+
+        if ($banner.length > 0 &&
+            $header.hasClass('alt')) {
+
+            $window.on('resize', function () {
+                $window.trigger('scroll');
+            });
+
+            $banner.scrollex({
+                bottom: $header.outerHeight() + 1,
+                terminate: function () {
+                    $header.removeClass('alt');
+                },
+                enter: function () {
+                    $header.addClass('alt');
+                },
+                leave: function () {
+                    $header.removeClass('alt');
+                }
+            });
+
+        }
+
+    });
+
+})(jQuery);
+/*slideer*/
+
+
+
+$(document).ready(function () {
+    $("#news-slider").owlCarousel({
+        items: 3,
+        itemsDesktop: [1199, 3],
+        itemsDesktopSmall: [980, 2],
+        itemsMobile: [600, 1],
+        navigation: true,
+        navigationText: ["", ""],
+        pagination: true,
+        autoPlay: true
+    });
+});
+
+/*accordion*/
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
